efe688bbae7ae50ac4f93647edf46faf45a48f54
Dauth Gurung <dauth22grg22@gmail.com>
2025-11-14 22:40:16 +1100

    chore(init): create Spring Boot 3.5 ecommerce project with base dependencies

---
+++ src/main/java/com/ebazar/ebazarapi/EbazarapiApplication.java
@@ -0,0 +1,13 @@
+package com.ebazar.ebazarapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EbazarapiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EbazarapiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ebazar/ebazarapi/EbazarapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ebazar.ebazarapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EbazarapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
