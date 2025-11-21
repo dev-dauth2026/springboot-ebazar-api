33d20cce18c0e9ee4b0f79e8f3df207d1740dc6a
Dauth Gurung <dauth22grg22@gmail.com>
2025-11-21 22:26:58 +1100

    Add reusable exception types for REST API

---
+++ src/main/java/com/ebazar/ebazarapi/exception/ResourceNotFoundException.java
@@ -0,0 +1,9 @@
+package com.ebazar.ebazarapi.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+	
+	public ResourceNotFoundException(String message) {
+        super(message);
+    }
+
+}
