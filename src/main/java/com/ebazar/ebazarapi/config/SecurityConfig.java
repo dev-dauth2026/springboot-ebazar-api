3c2e288fbfc2201fc7956700583eed542935e507
Dauth Gurung <dauth22grg22@gmail.com>
2025-11-22 11:36:31 +1100

    refactor(security): switch from in-memory users to CustomUserDetailsService for DB authentication

--- src/main/java/com/ebazar/ebazarapi/config/SecurityConfig.java
+++ src/main/java/com/ebazar/ebazarapi/config/SecurityConfig.java
@@ -3,7 +3,9 @@ package com.ebazar.ebazarapi.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -60,5 +62,11 @@ public class SecurityConfig {
     @Bean
     PasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
+    }
+    
+ // Needed for login processing
+    @Bean
+    AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
+        return config.getAuthenticationManager();
     }
 }
